073cecd18b32f7fadab4b941d2b0d07c6a9f048d
Nichole <nchnicolini@gmail.com>
2025-04-06 17:27:17 -0300

    view

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,7 @@ import WeatherDashboard from "./components/WeatherDashboards";
 
 function App() {
   return (
-    <div
-      className="h-screen w-screen bg-black text-white overflow-hidden"
-      style={{
-        paddingTop: "env(safe-area-inset-top)",
-        paddingBottom: "env(safe-area-inset-bottom)",
-        paddingLeft: "env(safe-area-inset-left)",
-        paddingRight: "env(safe-area-inset-right)",
-      }}
-    >
+    <div>
       <WeatherDashboard />
     </div>
   );
