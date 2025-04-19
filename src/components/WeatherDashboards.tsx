faa8eb6a2278bc84c78ea18e9bc1d96a08f3f39d
Nichole <nchnicolini@gmail.com>
2025-04-19 16:07:39 -0300

    atualizar o mongodv

--- src/components/WeatherDashboards.tsx
+++ src/components/WeatherDashboards.tsx
@@ -3,7 +3,6 @@ import { Home, Map, Star, CloudRainIcon } from "lucide-react";
 import AtualizarPrevisoes from "./AtualizarPrevisoes";
 import { motion } from "framer-motion";
 import { WeatherCarousel } from "./WeatherCarousel";
-import dotenv from "dotenv";
 
 interface Previsao {
   timestamp: string;
@@ -17,28 +16,23 @@ interface Previsao {
   location: string;
   weather_icon?: string;
 }
-dotenv.config();
 
 export default function WeatherDashboard() {
   const [activeTab, setActiveTab] = useState("daily");
   const [previsoes, setPrevisoes] = useState<Previsao[]>([]);
   const [loading, setLoading] = useState(false);
-  const token = process.env.REACT_APP_SECRET_TOKEN;
 
   const [horaAtual, setHoraAtual] = useState(new Date());
 
-  const atualizarPrevisoes = async () => {
+  /* const atualizarPrevisoes = async () => {
     try {
       await fetch("https://whitenights.onrender.com/weatherSave", {
         method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
       });
     } catch (erro) {
       console.error("Erro ao atualizar previsões:", erro);
     }
-  };
+  };*/
 
   const buscarPrevisoes = async () => {
     setLoading(true);
@@ -331,11 +325,6 @@ export default function WeatherDashboard() {
                 onAtualizar={buscarPrevisoes}
                 loading={loading}
               />
-            </div>
-            <div>
-              <button onClick={atualizarPrevisoes}>
-                Salvar previsões(atualiza banco de dados)
-              </button>
             </div>
           </div>
 
